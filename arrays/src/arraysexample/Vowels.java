f07d9c231d003dd2c1c2fc14a9548eb70ee1d68b
navyajayavarapu <60091934+navyajayavarapu@users.noreply.github.com>
2020-01-28 17:38:07 +0530

    Arraylist,exception and interface

--- arrays/src/arraysexample/Vowels.java
+++ arrays/src/arraysexample/Vowels.java
@@ -1,5 +1,22 @@
 package arraysexample;
-
-public class Vowels {
-
+import java.util.Scanner;
+public class Vowels 
+{
+	public static void main(String args[])
+	{
+		String s;int  count=0;
+		Scanner in=new Scanner(System.in);
+		s=in.nextLine();
+		for(int i=0;i<s.length();i++)
+		{
+			char ch=s.charAt(i);
+			if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+			{
+				count++;
+			}
+			
+		}
+		System.out.println(" number of vowels are:"+count);
+	}
+	
 }
